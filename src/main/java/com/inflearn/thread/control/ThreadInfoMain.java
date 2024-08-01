d6890339eceb3d114498ee57d342b5066c347fed
Jmboy713 <jimin713@naver.com>
2024-08-01 13:13:53 +0900

    info

---
+++ src/main/java/com/inflearn/thread/control/ThreadInfoMain.java
@@ -0,0 +1,24 @@
+package com.inflearn.thread.control;
+
+import com.inflearn.thread.start.HelloRunnable;
+import com.inflearn.util.MyLogger;
+
+import static com.inflearn.util.MyLogger.*;
+
+public class ThreadInfoMain {
+    public static void main(String[] args) {
+        Thread mainThread = Thread.currentThread();
+        log("main thread name: " + mainThread.getName());
+        log("main thread id: " + mainThread.getId());
+        log("main thread state: " + mainThread.getState());
+        log("main thread group: " + mainThread.getThreadGroup());
+        log("main thread priority: " + mainThread.getPriority());
+
+        Thread thread = new Thread(new HelloRunnable(), "helloRunnable");
+        log("thread name: " + thread.getName());
+        log("thread id: " + thread.getId());
+        log("thread state: " + thread.getState());
+        log("thread group: " + thread.getThreadGroup());
+        log("thread priority: " + thread.getPriority());
+    }
+}
